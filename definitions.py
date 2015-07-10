c30f9ec99eeabb3793cedfcc0521f6e9f0af99ec
voxei <dylan.kappler@gmail.com>
2015-07-09 19:32:59 -0700

    Profession Descriptions
    Added full text for prof. descriptions.  Tested functionality of loops
    to decide class.

--- definitions.py
+++ definitions.py
@@ -34,14 +34,47 @@ def start_console(shipname):
 def user_description(choice):
 	
 	if choice == "1":
-		print "The Pilot"
+		print "\n"
+		print "Pilot"
+		print "\n"
 		print "Good with ships, but that's about it.  Aggressive, controlled,"
 		print "and always pushing the limit."
 		print "Doesn't really understand fear, this is sometimes a downfall."
 		print "Can hold his/her own in a scrap."
 		print "SPECIAL: Ignores any negative ship status effects."
 		print "\nAre you sure you're a pilot?"
+	elif choice == "2":
+		print "\n"
+		print "Bounty Hunter"
+		print "\n"
+		print "Lord of gadgets, gizmos, etc for hunting humans in space."
+		print "Eye is always on the money and is quite selfish in most cases."
+		print "Does not trust many. Is a boss at fighting."
+		print "SPECIAL: Ignores any negative fight status effects."
+	elif choice == "3":
+		print "\n"
+		print "Medic"
+		print "\n"
+		print "Heals like a boss, cares deeply for his/her friends."
+		print "Trustworthy, honest, and calculated."
+		print "The Medic is a nice guy, sometimes too nice."
+		print "Scrawniest of the classes."
+		print "SPECIAL: Heals at a TBD rate every TBD amount of rooms."
+	elif choice == "4":
+		print "\n"
+		print "Scoundrel"
+		print "\n"
+		print "Lying, cheating, and sneaking are all traits of The Scoundrel."
+		print "Is very good at convincing others to bend to his/her will."
+		print "Forges alliances that benefit his/her power and influence."
+		print "SPECIAL: Can sneak past many foes unnoticed."
 
 def chosen_profession(prof_choice):
 	if prof_choice == "1":
 		print "I thought you looked Pilot-ey."
+	elif prof_choice == "2":
+		print "I thought you looked Bounty Hunter-ey."
+	elif prof_choice == "3":
+		print "I thought you looked Medic-ey."
+	elif prof_choice == "4":
+		print "I thought you looked Scoundrel-ey."
